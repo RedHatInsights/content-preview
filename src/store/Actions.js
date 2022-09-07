af3b0483dd0ea29cc2ae955a6fb35415fe96c41f
Chen lizhong <lichen@redhat.com>
2022-09-07 10:30:50 +0800

    Add customer server

--- src/store/Actions.js
+++ src/store/Actions.js
@@ -3,7 +3,28 @@ import * as ActionTypes from '../AppConstants';
 import API from '../Utilities/Api';
 
 const fetchData = async (url, headers, options) => {
+    if ((url.indexOf('localhost') > 0 || url.indexOf('custom') > 0) && url.indexOf('detail') < 1 && url.indexOf('hits') > 0)
+    {
+    const response_0 = await API.get('https://rehearse.usersys.redhat.com:8090/hits/', headers, options);
+    return response_0.data;
+    }
     const response = await API.get(url, headers, options);
+    if (url.indexOf('hits') > 0 && url.indexOf('detail') > 0)
+    {
+       if (url.indexOf('localhost') > 0)
+       {
+        const response2 = await API.get(url.replace('http://localhost:8080', 'https://rehearse.usersys.redhat.com:8090'), headers, options);
+    const merged = response.data.concat(response2.data);
+    return merged;
+      }
+      if (url.indexOf('custom') > 0)
+        {
+        const response2 = await API.get(url.replace('https://custom.server.org:8090', 'https://rehearse.usersys.redhat.com:8090'), headers, options);
+       const merged = response.data.concat(response2.data)
+       return merged;
+      }
+    }
+
     return response.data;
 };
 
